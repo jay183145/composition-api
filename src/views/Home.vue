a44f2ce471f8d9031141472bb17e76373c3f6791
藍天駿 <jay183145@gmail.com>
2022-05-12 11:07:59 +0800

    manipulating DOM

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 
 export default {
   name: 'Home',
   setup() {
-    console.log('setup')
 
+    const p = ref(null)
     let name = 'mario'
     let age = 30
 
     const handleClick = (e) => {
-      console.log('you clicked me')
+      console.log(p, p.value)
+      p.value.classList.add('DoM')
+      p.value.textContent ="I Made it!"
     }
 
-    return { name, age, handleClick }
+    return { name, age, handleClick, p }
   },
-  mounted() {
-    console.log('mounted')
-  }
 }
 </script>
